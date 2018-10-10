72673633987b3ff58b3e6226051e087bf479b13b
jacson-barth <jacsonm2@gmail.com>
2018-10-10 18:08:44 +0200

    mfc cmd run validated in flight (zagie)

---
+++ sw/airborne/firmwares/fixedwing/stabilization/stabilization_mfc.h
@@ -0,0 +1,56 @@
+/*
+ * This control algorithm is Model Free Control (MFC)
+ */
+
+#ifndef STABILIZATION_MFC_H
+#define STABILIZATION_MFC_H
+
+#include <inttypes.h>
+#include "std.h"
+#include "paparazzi.h"
+#include "generated/airframe.h"
+
+
+struct MfcParameters{
+  // Vector that keep the previous values
+  float setpoint;
+  float setpoint_trajec[3];
+  uint8_t use_trajec_sp;
+  float measure;
+  float error[3];
+  float command[2];
+  float estimator_num[3];
+  float estimator_den[3];
+  // Static values
+  float time_trajec;
+  float int_window;
+  float command_filter;
+  float alpha;
+  float kp;
+  float kd;
+};
+
+struct Mfc{
+  float sample_time;
+  float time;
+  float start_time;
+};
+
+// MFC structs
+extern struct Mfc mfc;
+extern struct MfcParameters mfc_roll;
+extern struct MfcParameters mfc_pitch;
+
+// PPRZ commands
+extern pprz_t mfc_aileron_setpoint_pprz;  // roll  -> aileron
+extern pprz_t mfc_elevator_setpoint_pprz; // pitch -> elevator
+
+// MFC functions
+extern void stabilization_mfc_init(void);
+extern void stabilization_mfc_init_time_state(void);
+extern void init_filters(void);
+extern void stabilization_mfc_set_failsafe_setpoint(void);
+extern int16_t stabilization_mfc_calc_cmd(struct MfcParameters *mfc_stt, float measure);
+extern void stabilization_mfc_run(void);
+
+#endif
